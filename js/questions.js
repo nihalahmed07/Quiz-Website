9df15e919df640f31fd7f687eb3bada7176676dd
Nihal Ahmed N <n.nihalahmed1@gmail.com>
2025-04-26 18:07:58 +0530

    Create questions.js

---
+++ js/questions.js
@@ -0,0 +1,192 @@
+const questions = [
+  {
+    numb: 1,
+    question: "What does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Preprocessor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language"
+    ]
+  },
+  {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Which HTML tag is used to define an internal style sheet?",
+    answer: "<style>",
+    options: ["&lt;script&gt;", "&lt;style&gt;", "&lt;css&gt;", "&lt;link&gt;"]
+  },
+  {
+    numb: 4,
+    question: "Which property is used to change the background color in CSS?",
+    answer: "background-color",
+    options: ["color", "bgcolor", "background-color", "background"]
+  },
+  {
+    numb: 5,
+    question: "Inside which HTML element do we put the JavaScript?",
+    answer: "<script>",
+    options: ["&lt;javascript&gt;", "&lt;script&gt;", "&lt;js&gt;", "&lt;scripting&gt;"]
+  },
+  {
+    numb: 6,
+    question: "Which CSS property controls the text size?",
+    answer: "font-size",
+    options: ["text-style", "text-size", "font-size", "font-style"]
+  },
+  {
+    numb: 7,
+    question: "What does DOM stand for?",
+    answer: "Document Object Model",
+    options: [
+      "Data Object Model",
+      "Document Object Model",
+      "Desktop Object Model",
+      "Display Object Management"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which HTML attribute is used to define inline styles?",
+    answer: "style",
+    options: ["class", "style", "styles", "font"]
+  },
+  {
+    numb: 9,
+    question: "Which property is used to make text bold in CSS?",
+    answer: "font-weight",
+    options: ["bold", "font-bold", "font-weight", "text-bold"]
+  },
+  {
+    numb: 10,
+    question: "How do you write 'Hello World' in an alert box?",
+    answer: "alert('Hello World');",
+    options: [
+      "msg('Hello World');",
+      "alert('Hello World');",
+      "msgBox('Hello World');",
+      "alertBox('Hello World');"
+    ]
+  },
+  {
+    numb: 11,
+    question: "Which tag is used to insert a line break in HTML?",
+    answer: "<br>",
+    options: ["&lt;break&gt;", "&lt;br&gt;", "&lt;lb&gt;", "&lt;line&gt;"]
+  },
+  {
+    numb: 12,
+    question: "Which symbol is used for comments in JavaScript?",
+    answer: "//",
+    options: ["&lt;!-- --&gt;", "//", "**", "/* */"]
+  },
+  {
+    numb: 13,
+    question: "Which CSS property is used to center text?",
+    answer: "text-align",
+    options: ["align", "text-align", "center", "margin"]
+  },
+  {
+    numb: 14,
+    question: "How do you add a comment in CSS?",
+    answer: "/* comment */",
+    options: ["// comment", "&lt;!-- comment --&gt;", "# comment", "/* comment */"]
+  },
+  {
+    numb: 15,
+    question: "Which HTML element defines the title of a document?",
+    answer: "<title>",
+    options: ["&lt;meta&gt;", "&lt;head&gt;", "&lt;title&gt;", "&lt;header&gt;"]
+  },
+  {
+    numb: 16,
+    question: "Which event occurs when the user clicks on an HTML element?",
+    answer: "onclick",
+    options: ["onmouseclick", "onchange", "onclick", "onmouseover"]
+  },
+  {
+    numb: 17,
+    question: "Which method is used to access an element by ID in JS?",
+    answer: "getElementById()",
+    options: [
+      "getElement(id)",
+      "getElementById()",
+      "querySelectorAll()",
+      "getById()"
+    ]
+  },
+  {
+    numb: 18,
+    question: "Which input type defines a checkbox in HTML?",
+    answer: "checkbox",
+    options: ["toggle", "checkbox", "radio", "check"]
+  },
+  {
+    numb: 19,
+    question: "How do you create a function in JavaScript?",
+    answer: "function myFunction()",
+    options: [
+      "function:myFunction()",
+      "function = myFunction()",
+      "function myFunction()",
+      "def myFunction()"
+    ]
+  },
+  {
+    numb: 20,
+    question: "Which CSS property is used to make a website responsive?",
+    answer: "media queries",
+    options: [
+      "responsive",
+      "screen-fit",
+      "media queries",
+      "fit-device"
+    ]
+  },
+  {
+    numb: 21,
+    question: "What does 'var' stand for in JavaScript?",
+    answer: "variable",
+    options: ["variant", "variety", "variable", "vary"]
+  },
+  {
+    numb: 22,
+    question: "Which HTML tag is used to display a table?",
+    answer: "<table>",
+    options: ["&lt;table&gt;", "&lt;td&gt;", "&lt;tab&gt;", "&lt;tbl&gt;"]
+  },
+  {
+    numb: 23,
+    question: "Which attribute specifies the path to an image?",
+    answer: "src",
+    options: ["link", "href", "src", "path"]
+  },
+  {
+    numb: 24,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Color Style Sheet",
+      "Cascading Style Sheet",
+      "Creative Style Sheet",
+      "Computed Style Sheet"
+    ]
+  },
+  {
+    numb: 25,
+    question: "Which JavaScript method converts JSON to an object?",
+    answer: "JSON.parse()",
+    options: ["JSON.parse()", "JSON.stringify()", "JSON.object()", "JSON.convert()"]
+  }
+];
